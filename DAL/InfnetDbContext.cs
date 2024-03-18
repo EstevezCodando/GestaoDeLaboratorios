9c1e1dd970591e966d5c3757100ecdf8d69ea330
Jean <jean.alvarez@al.infnet.edu.br>
2024-03-17 23:48:49 -0300

    Crud computadores MVC

---
+++ DAL/InfnetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GestaoDeLaboratorios.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestaodeLaboratorio.DAL
+{
+    public class INFNETDbContext : DbContext
+    {
+        public INFNETDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Computadores> Computadores { get; set; }
+
+    }
+}
